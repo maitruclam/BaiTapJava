bf4ff220eef81bd5a33c1170fa652f0b313ec9f3
Mai Trúc Lâm <55437212+lamtitanqq@users.noreply.github.com>
2019-09-29 20:44:15 +0700

    Lab 1 done - lab 2 đang chạy - lab 3 chưa xong T^T

---
+++ Tuan 3/Lab 1/src/Bài1/JFrame_canban.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bài1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class JFrame_canban extends JFrame{
+    public JFrame_canban(){
+        setTitle("Wellcome to Hagaka");
+        setSize(400, 400);
+        setVisible(true);
+        
+        
+    }
+
+    public static void main(String[] agrs) {
+        new JFrame_canban();
+                
+    }
+
+}
